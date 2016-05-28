ac8ba55148c988589e002b867caa908e673650b8
bluMyst <weirdusername@gmail.com>
2016-05-28 18:22:10 -0500

    added element_children and table_extend to lib

--- ahtolib.lua
+++ ahtolib.lua
@@ -1,3 +1,4 @@
+local std = require('std._base')
 local ahtolib = { }
 ahtolib.DEBUG = false
 ahtolib.debug = function(player, string)
@@ -14,5 +15,38 @@ ahtolib.gui_tostring = function(top_element)
   end
   local s = table.concat(std.ireverse(elements), '/')
   return s
+end
+ahtolib.table_extend = function(table1, table2, destructive)
+  if destructive == nil then
+    destructive = false
+  end
+  if not destructive then
+    local new_table = std.table.clone(table1)
+  else
+    local new_table = table1
+  end
+  for i in table2 do
+    new_table[#new_table + 1] = i
+  end
+  return new_table
+end
+ahtolib.element_children = function(element, recursive)
+  if recursive == nil then
+    recursive = true
+  end
+  local children = { }
+  local _list_0 = element.children_names
+  for _index_0 = 1, #_list_0 do
+    local child_name = _list_0[_index_0]
+    children[#children + 1] = element[child_name]
+  end
+  if not recursive then
+    return children
+  end
+  for _index_0 = 1, #children do
+    local child = children[_index_0]
+    local children_of_child = ahtolib.element_children(child)
+    ahtolib.table_extend(children, children_of_child, true)
+  end
 end
 return ahtolib
--- ahtolib.moon
+++ ahtolib.moon
@@ -1,3 +1,5 @@
+std = require 'std._base'
+
 ahtolib = {}
 
 ahtolib.DEBUG = false
@@ -17,5 +19,28 @@ ahtolib.gui_tostring = (top_element) ->
     s = table.concat(std.ireverse(elements), '/')
 
     return s
+
+ahtolib.table_extend = (table1, table2, destructive=false) ->
+    if not destructive
+        new_table = std.table.clone table1
+    else
+        new_table = table1
+
+    for i in table2
+        new_table[#new_table+1] = i
+
+    return new_table
+
+ahtolib.element_children = (element, recursive=true) ->
+    children = {}
+
+    for child_name in *element.children_names
+        children[#children+1] = element[child_name]
+
+    if not recursive then return children
+
+    for child in *children
+        children_of_child = ahtolib.element_children(child)
+        ahtolib.table_extend children, children_of_child, true
 
 return ahtolib
