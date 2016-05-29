ca3c8e2c9fe57abee525d09ea630817ffb69fc0b
bluMyst <weirdusername@gmail.com>
2016-05-28 23:00:13 -0500

    Sooo many changes to control.moon

--- control.lua
+++ control.lua
@@ -1,8 +1,51 @@
 local std = require('std._base')
-local ahtolib = require('ahtolib')
+local ahto = require('ahtolib')
 require('defines')
 local DEBUG = true
-ahtolib.DEBUG = DEBUG
+ahto.DEBUG = DEBUG
+local gui_events = { }
+local element_on_click
+element_on_click = function(element, on_click)
+  local element_s = ahto.gui_tostring(element)
+  gui_events[element_s] = {
+    on_click = on_click
+  }
+end
+local element_destroy
+element_destroy = function(element)
+  local element_s = ahto.gui_tostring(element)
+  gui_events[element_s] = nil
+  local _list_0 = ahto.element_children(element)
+  for _index_0 = 1, #_list_0 do
+    local i = _list_0[_index_0]
+    gui_events[i] = nil
+  end
+  return element.destroy()
+end
+local gui_click
+gui_click = function(event, element)
+  if element == nil then
+    element = event.element
+  end
+  local player = game.players[event.player_index]
+  local element_s = ahto.gui_tostring(element)
+  if DEBUG then
+    player.print("click -> " .. tostring(element_s))
+    if gui_events[element_s] == nil then
+      ahto.debug(player, "gui_events[" .. tostring(element_s) .. "] is nil, so set to {}")
+    end
+  end
+  gui_events[element_s] = gui_events[element_s] or { }
+  if gui_events[element_s].on_click then
+    gui_events[element_s].on_click(event, player)
+    local event_ran = true
+  else
+    local event_ran = false
+    ahto.debug(player, "Unable to find event for " .. tostring(element_s))
+  end
+  return event_ran
+end
+script.on_event(defines.events.on_gui_click, gui_click)
 local command_run
 command_run = function(player)
   local console_frame = player.gui.top.console_frame
@@ -10,15 +53,40 @@ command_run = function(player)
   player.print(">>> " .. tostring(command))
   command = loadstring("return " .. tostring(command)) or loadstring(command)
   local success, return_ = pcall(command)
-  if success then
-    player.print(std.tostring(return_))
-  else
-    player.print("ERROR: " .. tostring(return_))
+  local return_s = std.tostring(return_)
+  if not success then
+    return_s = "ERROR: " .. tostring(return_s)
   end
-  console_frame.command.text = ''
+  if return_ ~= nil then
+    return player.print(return_s)
+  end
 end
-local make_gui
-make_gui = function(player)
+local make_console_icon
+make_console_icon = function(player)
+  player.gui.top.add({
+    type = 'button',
+    name = 'open_console',
+    caption = '#'
+  })
+  return element_on_click(player.gui.top.open_console, function(event, player)
+    remote.call('blumisc', 'close_console_icon', player)
+    return remote.call('blumisc', 'make_console', player)
+  end)
+end
+local close_console_icon
+close_console_icon = function(player)
+  local icon = player.gui.top.open_console
+  local valid
+  if icon == nil then
+    valid = 'nil'
+  else
+    valid = std.tostring(icon.valid)
+  end
+  ahto.debug(player, "Destroying open_console button. Valid: " .. tostring(valid))
+  return element_destroy(icon)
+end
+local make_console
+make_console = function(player)
   player.gui.top.add({
     type = 'frame',
     name = 'console_frame',
@@ -35,62 +103,48 @@ make_gui = function(player)
     name = 'button_flow',
     direction = 'horizontal'
   })
-  console_frame.button_flow.add({
+  local button_flow = console_frame.button_flow
+  button_flow.add({
     type = 'button',
     name = 'submit',
     caption = 'submit'
   })
-  if debug then
-    console_frame.button_flow.add({
-      type = 'button',
-      name = 'reset',
-      caption = 'reset'
-    })
-  else
-    console_frame.button_flow.add({
-      type = 'button',
-      name = 'clear',
-      caption = 'clear'
-    })
-  end
-  console_frame.button_flow.add({
+  element_on_click(button_flow.submit, function(event, player)
+    remote.call('blumisc', 'command_run', player)
+    player.gui.top.console_frame.command.text = ''
+  end)
+  button_flow.add({
+    type = 'button',
+    name = 'clear',
+    caption = 'clear'
+  })
+  element_on_click(button_flow.clear, function(event, player)
+    player.gui.top.console_frame.command.text = ''
+  end)
+  button_flow.add({
     type = 'button',
     name = 'close',
     caption = 'close'
   })
-  return script.on_event(defines.events.on_gui_click, function(event)
-    player = game.players[event.player_index]
-    console_frame = player.gui.top.console_frame
-    local _exp_0 = event.element
-    if console_frame.button_flow.submit == _exp_0 then
-      remote.call('blumisc', 'command_run', player)
-    elseif console_frame.button_flow.close == _exp_0 then
-      remote.call('blumisc', 'destroy_gui', player)
-    elseif console_frame.button_flow.reset == _exp_0 then
-      remote.call('blumisc', 'destroy_gui', player)
-      remote.call('blumisc', 'make_gui', player)
-    elseif console_frame.button_flow.clear == _exp_0 then
-      console_frame.command.text = ''
-    end
-    if DEBUG then
-      return player.print("click -> " .. tostring(ahtolib.gui_tostring(event.element)))
-    end
+  return element_on_click(button_flow.close, function(event, player)
+    remote.call('blumisc', 'close_console', player)
+    return remote.call('blumisc', 'make_console_icon', player)
   end)
 end
-local destroy_gui
-destroy_gui = function(player)
-  script.on_event(defines.events.on_gui_click, nil)
-  return player.gui.top.console_frame.destroy()
+local close_console
+close_console = function(player)
+  return element_destroy(player.gui.top.console_frame)
 end
 remote.add_interface('blumisc', {
-  make_gui = make_gui,
-  destroy_gui = destroy_gui,
+  make_console = make_console,
+  close_console = close_console,
+  make_console_icon = make_console_icon,
+  close_console_icon = close_console_icon,
   command_run = command_run
 })
-return script.on_init(function()
-  local _list_0 = game.players
-  for _index_0 = 1, #_list_0 do
-    local player = _list_0[_index_0]
-    make_gui(player)
+return script.on_event(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+  if not player.gui.top.open_console and not player.gui.top.console_frame then
+    return make_console_icon(player)
   end
 end)
--- control.moon
+++ control.moon
@@ -1,10 +1,52 @@
-std     = require 'std._base'
-ahtolib = require 'ahtolib'
+std  = require 'std._base'
+ahto = require 'ahtolib'
 require 'defines'
 
 -- moonscript doesn't have "a = b = c" syntax
 DEBUG = true
-ahtolib.DEBUG = DEBUG
+ahto.DEBUG = DEBUG
+
+-- {gui_element: {on_click:f, on_asdf:g}}
+gui_events = {}
+
+element_on_click = (element, on_click) ->
+    element_s = ahto.gui_tostring element
+    gui_events[element_s] = on_click: on_click
+
+element_destroy = (element) ->
+    element_s = ahto.gui_tostring element
+    gui_events[element_s] = nil
+
+    for i in *ahto.element_children element
+        gui_events[i] = nil
+        -- don't destroy the children because destroying the parent element will
+        -- automatically do this for us.
+
+    element.destroy()
+
+gui_click = (event, element=event.element) ->
+    -- on_click(event, player)
+    player = game.players[event.player_index]
+    element_s = ahto.gui_tostring element
+
+    if DEBUG
+        player.print "click -> #{element_s}"
+
+        if gui_events[element_s] == nil
+            ahto.debug player, "gui_events[#{element_s}] is nil, so set to {}"
+
+    gui_events[element_s] or= {}
+
+    if gui_events[element_s].on_click
+        gui_events[element_s].on_click(event, player)
+        event_ran = true
+    else
+        event_ran = false
+        ahto.debug player, "Unable to find event for #{element_s}"
+
+    return event_ran
+
+script.on_event defines.events.on_gui_click, gui_click
 
 command_run = (player) ->
     console_frame = player.gui.top.console_frame
@@ -17,14 +59,29 @@ command_run = (player) ->
     command = loadstring("return #{command}") or loadstring(command)
     success, return_ = pcall(command)
 
-    if success
-        player.print std.tostring return_
-    else
-        player.print "ERROR: #{return_}"
+    return_s = std.tostring return_
+    if not success then return_s = "ERROR: #{return_s}"
+    player.print return_s if return_ != nil
 
-    console_frame.command.text = ''
+make_console_icon = (player) ->
+    -- won't look like an icon at first, but should be after styling it to be
+    -- tiny
+    player.gui.top.add
+        type: 'button'
+        name: 'open_console'
+        caption: '#'
+    element_on_click player.gui.top.open_console, (event, player) ->
+        remote.call 'blumisc', 'close_console_icon', player
+        remote.call 'blumisc', 'make_console', player
 
-make_gui = (player) ->
+close_console_icon = (player) ->
+    icon = player.gui.top.open_console
+    valid = if icon == nil then 'nil' else std.tostring(icon.valid)
+    ahto.debug player, "Destroying open_console button. Valid: #{valid}"
+    element_destroy icon
+
+make_console = (player) ->
+    -- I checked; it's not possible to add a custom key to a GuiElement.
     player.gui.top.add
         type:       'frame'
         name:       'console_frame'
@@ -42,58 +99,44 @@ make_gui = (player) ->
         name:       'button_flow'
         direction:  'horizontal'
 
-    console_frame.button_flow.add
+    button_flow = console_frame.button_flow
+
+    button_flow.add
         type:     'button'
         name:     'submit'
         caption:  'submit'
+    element_on_click button_flow.submit, (event, player) ->
+        -- can't just call it because of weird namespace problem
+        remote.call 'blumisc', 'command_run', player
+        player.gui.top.console_frame.command.text = ''
 
-    if debug
-        console_frame.button_flow.add
-            type:     'button'
-            name:     'reset'
-            caption:  'reset'
-    else
-        console_frame.button_flow.add
-            type:     'button'
-            name:     'clear'
-            caption:  'clear'
+    button_flow.add
+        type:     'button'
+        name:     'clear'
+        caption:  'clear'
+    element_on_click button_flow.clear, (event, player) ->
+        player.gui.top.console_frame.command.text = ''
 
-    console_frame.button_flow.add
+    button_flow.add
         type:     'button'
         name:     'close'
         caption:  'close'
+    element_on_click button_flow.close, (event, player) ->
+        remote.call 'blumisc', 'close_console', player
+        remote.call 'blumisc', 'make_console_icon' ,player
 
-    -- TODO: Run this on_init
-    script.on_event defines.events.on_gui_click, (event) ->
-        player = game.players[event.player_index]
-        console_frame = player.gui.top.console_frame
-
-        switch event.element
-            when console_frame.button_flow.submit
-                -- can't just call it because of weird namespace problem
-                remote.call('blumisc', 'command_run', player)
-            when console_frame.button_flow.close
-                remote.call('blumisc', 'destroy_gui', player)
-            when console_frame.button_flow.reset
-                remote.call('blumisc', 'destroy_gui', player)
-                remote.call('blumisc', 'make_gui', player)
-            when console_frame.button_flow.clear
-                console_frame.command.text = ''
-
-        if DEBUG
-            player.print "click -> #{ahtolib.gui_tostring(event.element)}"
-
-destroy_gui = (player) ->
-    script.on_event defines.events.on_gui_click, nil
-    player.gui.top.console_frame.destroy()
+close_console = (player) ->
+    element_destroy player.gui.top.console_frame
 
 remote.add_interface 'blumisc', {
-    make_gui:     make_gui,
-    destroy_gui:  destroy_gui,
-    command_run:  command_run}
---  get: -> {make_gui:make_gui, destroy_gui:destroy_gui}}
--- can't copy object of type function
+    make_console:        make_console,
+    close_console:       close_console,
+    make_console_icon:   make_console_icon,
+    close_console_icon:  close_console_icon,
+    command_run:         command_run}
 
-script.on_init ->
-    for player in *game.players
-        make_gui player
+--script.on_init ->
+script.on_event defines.events.on_player_created, (event) ->
+    player = game.players[event.player_index]
+    if not player.gui.top.open_console and not player.gui.top.console_frame
+        make_console_icon player
